01fe5467b42df983d0df19fefc632e6f5a808c4f
piotr233 <pdrabik223@gmail.com>
2023-08-23 15:30:25 +0200

    markdown

--- lib/information.dart
+++ lib/information.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:markdown/markdown.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Information extends StatefulWidget {
   const Information({super.key});
@@ -25,8 +31,24 @@ class _Information extends State<Information> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-            "Information view is not yet implemented, but the current version is :$version ",
-            style: Theme.of(context).textTheme.headlineMedium));
+        child: FutureBuilder(
+            future: rootBundle.loadString("welcome.md"),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                return Markdown(
+                  data: snapshot.data!,
+                  selectable: false,
+                  onTapLink: (text, url, title) {
+                    launchUrl(
+                        Uri.parse(url!)); /*For url_launcher 6.1.0 and higher*/
+                    // launch(url);  /*For url_launcher 6.0.20 and lower*/
+                  },
+                );
+              }
+
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }));
   }
 }
