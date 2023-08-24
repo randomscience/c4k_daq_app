fe6b6ce2878ff030acf51f9a1a2cbb21b517ea6a
piotr233 <pdrabik223@gmail.com>
2023-08-24 20:49:49 +0200

    styling is kinda done

--- lib/library_card.dart
+++ lib/library_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class LibraryCard extends StatefulWidget {
   final Map<String, dynamic> localJsonData;
@@ -10,40 +11,76 @@ class LibraryCard extends StatefulWidget {
 
 class _LibraryCard extends State<LibraryCard> {
   String id = "";
-  String measurementTime = "";
+
+  String measurementTimeDay = "";
+  String measurementTimeHour = "";
+
   @override
   void initState() {
     super.initState();
     id = widget.localJsonData['id'];
-    measurementTime = widget.localJsonData['measurement_time'];
+
+    final DateTime date =
+        DateTime.parse(widget.localJsonData['measurement_time']);
+
+    measurementTimeDay = DateFormat('dd-MM-yyyy').format(date.toLocal());
+    measurementTimeHour = DateFormat('H:mm').format(date.toLocal());
+
+    // .parseUTC(widget.localJsonData['measurement_time'])
+    // .day
+    // .toString();
   }
 
   void _retryUpload() {}
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-          child: Column(children: [
-        Padding(
-            padding: const EdgeInsets.fromLTRB(8, 6, 6, 6),
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Id: $id", style: const TextStyle(fontSize: 24)))),
-        Padding(
-            padding: const EdgeInsets.fromLTRB(8, 6, 6, 6),
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Date: $measurementTime",
-                    style: const TextStyle(fontSize: 16)))),
-        Padding(
-            padding: const EdgeInsets.fromLTRB(8, 6, 6, 6),
-            child: Align(
-                alignment: Alignment.bottomRight,
-                child: ElevatedButton(
-                    onPressed: () => {_retryUpload()},
-                    child: const Text('Retry upload'))))
-      ])),
-    );
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(8, 0, 8, 6),
+        child: Center(
+          child: Card(
+              child: Column(children: [
+            Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 6, 6),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("ID: $id",
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)))),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(16, 6, 6, 0),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        "Stworzono: $measurementTimeDay o godzinie: $measurementTimeHour",
+                        style: const TextStyle(fontSize: 16)))),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 8, 10),
+                    child: TextButton(
+                      // style: ElevatedButton.styleFrom(
+                      //     backgroundColor: Colors.transparent),
+                      onPressed: () => {},
+                      child: const Text(
+                        'Usuń',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 8, 10),
+                  child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueAccent),
+                          onPressed: () => {_retryUpload()},
+                          child: const Text('Wyślij',
+                              style: TextStyle(color: Colors.white)))))
+            ])
+          ])),
+        ));
   }
 }
